6ed4c040d3be55d4af7fe71c9a05dd39c3271057
Mark Perkins <mark@allmarkedup.com>
2016-03-08 23:10:39 +0000

    Add handlebars for layout rendering

--- adapter.js
+++ adapter.js
@@ -1,8 +1,9 @@
 'use strict';
 
-const React    = require('react');
-const ReactDOM = require('react-dom/server');
-const _        = require('lodash');
+const Handlebars = require('handlebars');
+const React      = require('react');
+const ReactDOM   = require('react-dom/server');
+const _          = require('lodash');
 
 module.exports = function(source, config){
 
@@ -28,12 +29,16 @@ module.exports = function(source, config){
     // source.on('changed', loadViews);
 
     return {
-        render: function(path, str, context, meta){
+        render: function(path, str, context){
             delete require.cache[path];
             const component = require(path);
             const element   = React.createElement(component, context);
-            const html      = ReactDOM.renderToStaticMarkup(el);
+            const html      = ReactDOM.renderToStaticMarkup(element);
             return Promise.resolve(html);
+        },
+        renderLayout: function(path, str, context){
+            const template = Handlebars.compile(str);
+            return Promise.resolve(template(context));
         }
     }
 };
