aa8ae3ea52d42182b78fb2a6b78b1f9652b9e356
Mark Perkins <mark@allmarkedup.com>
2016-06-27 22:02:35 +0100

    Update to v1.0 syntax

--- adapter.js
+++ adapter.js
@@ -5,27 +5,68 @@ require("babel-register")({
     presets: ['react', 'es2015']
 });
 
+
 const Handlebars = require('handlebars');
 const React      = require('react');
 const ReactDOM   = require('react-dom/server');
+const Promise    = require('bluebird');
+const Adapter    = require('@frctl/fractal').Adapter;
 
-module.exports = function(source, config){
+
+class NunjucksAdapter extends Adapter {
+
+    constructor(source, loadPaths) {
+
+        super(null, source);
+
+    }
+
+    render(path, str, context){
+        delete require.cache[path];
+        const component = require(path);
+        const element   = React.createElement(component, context);
+        const html      = ReactDOM.renderToStaticMarkup(element);
+        return Promise.resolve(html);
+    }
+
+    renderLayout(path, str, context){
+        const template = Handlebars.compile(str);
+        return Promise.resolve(template(context));
+    }
+
+}
+
+module.exports = function(config) {
 
     config = config || {};
 
     return {
 
-        render: function(path, str, context){
-            delete require.cache[path];
-            const component = require(path);
-            const element   = React.createElement(component, context);
-            const html      = ReactDOM.renderToStaticMarkup(element);
-            return Promise.resolve(html);
-        },
-
-        renderLayout: function(path, str, context){
-            const template = Handlebars.compile(str);
-            return Promise.resolve(template(context));
+        register(source, app) {
+            return new NunjucksAdapter(source);
         }
     }
+
 };
+
+
+// module.exports = function(source, config){
+//
+//     config = config || {};
+//
+//     return {
+//
+//         render: function(path, str, context){
+//             delete require.cache[path];
+//             const component = require(path);
+//             const element   = React.createElement(component, context);
+//             const html      = ReactDOM.renderToStaticMarkup(element);
+//             return Promise.resolve(html);
+//         },
+//
+//         renderLayout: function(path, str, context){
+//             const template = Handlebars.compile(str);
+//             return Promise.resolve(template(context));
+//         }
+//     }
+// };
