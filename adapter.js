cb41989ba4e0db6db420656d14bc303f11fcd5d6
Mark Perkins <mark@allmarkedup.com>
2016-06-28 11:27:14 +0100

    Cleanup

--- adapter.js
+++ adapter.js
@@ -5,20 +5,16 @@ require("babel-register")({
     presets: ['react', 'es2015']
 });
 
-
 const Handlebars = require('handlebars');
 const React      = require('react');
 const ReactDOM   = require('react-dom/server');
 const Promise    = require('bluebird');
 const Adapter    = require('@frctl/fractal').Adapter;
 
-
-class NunjucksAdapter extends Adapter {
+class ReactAdapter extends Adapter {
 
     constructor(source, loadPaths) {
-
         super(null, source);
-
     }
 
     render(path, str, context){
@@ -38,35 +34,13 @@ class NunjucksAdapter extends Adapter {
 
 module.exports = function(config) {
 
-    config = config || {};
+    config = config || {}; // not doing anything with config right now!
 
     return {
 
         register(source, app) {
-            return new NunjucksAdapter(source);
+            return new ReactAdapter(source);
         }
     }
 
 };
-
-
-// module.exports = function(source, config){
-//
-//     config = config || {};
-//
-//     return {
-//
-//         render: function(path, str, context){
-//             delete require.cache[path];
-//             const component = require(path);
-//             const element   = React.createElement(component, context);
-//             const html      = ReactDOM.renderToStaticMarkup(element);
-//             return Promise.resolve(html);
-//         },
-//
-//         renderLayout: function(path, str, context){
-//             const template = Handlebars.compile(str);
-//             return Promise.resolve(template(context));
-//         }
-//     }
-// };
